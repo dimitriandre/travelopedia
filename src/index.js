3eaa791403cd84f94ced4b1accf77ad85703914d
Dimitri Andre <dimitri@ymagyn.com>
2023-04-13 13:38:14 +0200

    get the data in the destination list view

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./layout/Header";
 import DestinationIndex from "./components/DestinationIndex";
+import DestinationList from "./components/DestinationList";
+import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
+import { destinationAPI } from "./api/destinationApi";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Header />
-    <DestinationIndex />
+    <ApiProvider api={destinationAPI}>
+      <Header />
+      <DestinationIndex />
+      <DestinationList />
+    </ApiProvider>
   </React.StrictMode>
 );
